f85fa1156ad78b9c4b78d388bac41c219da19014
zole011 <zoran.it@gmail.com>
2025-04-12 02:33:30 +0200

    new commit

---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:pep/Configuration/Sets/SitePackage/PageTsConfig/ContentElements/simpleteaser.typoscript'"
+);
